15a5da8a7042b7ffa40b725d46679295a2ce8729
k-kinzal <log@logn.in>
2015-07-26 23:05:39 +0900

    support retrying

--- install.js
+++ install.js
@@ -2,24 +2,31 @@
 
 var Promise = require('bluebird');
 
-Promise.resolve().then(function() {
-  return require('./src/scalajsc')
-           .runAsync(['-version'])
-           .then(function() {
-             console.log('scalajsc binary install successfully')
-           });
-}).then(function() {
-  return require('./src/scalajsc')
-           .runAsync(['-version'])
-           .then(function() {
-             console.log('scalajsld binary install successfully')
-           });
-}).then(function() {
-  return require('./src/scalajsc')
-           .runAsync(['-version'])
-           .then(function() {
-             console.log('scalajsp binary install successfully')
-           });
-}).catch(function(err) {
-  throw err;
-});
+(function install(retry) {
+  Promise.resolve().then(function() {
+    return require('./src/scalajsc')
+             .runAsync(['-version'])
+             .then(function() {
+               console.log('scalajsc binary install successfully')
+             });
+  }).then(function() {
+    return require('./src/scalajsc')
+             .runAsync(['-version'])
+             .then(function() {
+               console.log('scalajsld binary install successfully')
+             });
+  }).then(function() {
+    return require('./src/scalajsc')
+             .runAsync(['-version'])
+             .then(function() {
+               console.log('scalajsp binary install successfully')
+             });
+  }).catch(function(err) {
+    if (retry < 3) {
+      console.warn('Retrying: ' + err);
+      setTimeout(install, 3000, retry+1);
+      return;
+    }
+    console.error(err);
+  });
+})(0);
