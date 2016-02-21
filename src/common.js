4e4dd95aa5ba2526f76f4eb2b93d77b9728b11c6
k-kinzal <log@logn.in>
2016-02-21 23:18:52 +0900

    support 0.6.7

--- src/common.js
+++ src/common.js
@@ -7,7 +7,7 @@ var path = require('path');
 
 module.exports = function () {
   var scalaVersion = '2.11';
-  var scalaJsVersion = '0.6.4';
+  var scalaJsVersion = '0.6.7';
   
   var bin = new BinWrapper({strip: 0})
     .src('http://www.scala-js.org/files/scalajs_' + scalaVersion + '-' + scalaJsVersion + '.tgz', 'darwin')
